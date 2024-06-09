3616ef6ff5a86ad444020d43ac16fad813b0f270
object-Object <object@objectobject.ca>
2024-06-09 17:11:10 -0400

    Add non-flattening Json5 transmogrifier

--- buildSrc/src/main/kotlin/hexbound/json5.gradle.kts
+++ buildSrc/src/main/kotlin/hexbound/json5.gradle.kts
@@ -1,8 +1,7 @@
 package hexbound
 
 import at.petrak.pkpcpbp.filters.FlatteningJson5Transmogrifier
-import org.gradle.kotlin.dsl.filter
-import org.gradle.kotlin.dsl.withType
+import at.petrak.pkpcpbp.filters.Json5Transmogrifier
 
 // https://github.com/gamma-delta/PKPCPBP/blob/786194a590/src/main/java/at/petrak/pkpcpbp/PKSubprojPlugin.java#L84
 tasks.withType<ProcessResources>().configureEach {
@@ -11,5 +10,10 @@ tasks.withType<ProcessResources>().configureEach {
     filesMatching("**/*.flatten.json5") {
         path = path.replace(".flatten.json5", ".json")
         filter<FlatteningJson5Transmogrifier>()
+    }
+
+    filesMatching("**/*.json5") {
+        path = path.replace(".json5", ".json")
+        filter<Json5Transmogrifier>()
     }
 }
